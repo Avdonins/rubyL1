45601caacdfa1f82ff8a5b16e5e70cd5ce4eda86
avdonins <karwakacica@mail.ru>
2022-10-16 14:27:11 +0300

    hola, mundo!(another one)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
 
     def hello
-        render plain: "hola, mundo!"
+        render plain: "¡Hola, mundo!"
     end
 end
